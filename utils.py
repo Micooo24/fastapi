fd54f338b62461570a3bfdb25d93650ffc92cdc2
Micooo24 <mico.rabino24@gmail.com>
2025-01-24 09:50:04 +0800

    first commit

---
+++ utils.py
@@ -0,0 +1,15 @@
+import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "b6562e66f1c68ffe24fa"
+ALGORITHM = "HS256"
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
